26d1ca1cb1fe9e588ea98bdcf8132390be147235
jonasH <jonas.hesselryd@gmail.com>
2014-04-08 20:57:03 +0200

    Decent database working, or not?

--- lib/db.py
+++ lib/db.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QTableView
 from PyQt5.QtSql import (QSqlQuery, QSqlDatabase)
 
 
 def openDatabase(databaseName='fmdb.sqlite'):
     db = QSqlDatabase.addDatabase('QSQLITE') 
-    db.setDatabaseName(databaseName)
+#TODO: make this a variable
+    db.setDatabaseName(':memory:')
     if not db.open():
          QMessageBox.critical(None," OMG ERROR! withz the database: " + databaseName,
                 QMessageBox.Cancel)
          return False
-    return db
-
-
+#    query = QSqlQuery()
+#    for i in open("/home/jonas/Kod/FaktureraMera2/misc/fakturabackup.sqlite"):
+ #       query.exec_(i)
+ #       print(query.lastError().text())
     
+#    result = query.exec_('select * from bill')
+#    print(result)
+    return True
 
-if __name__ == '__main__':
-    import sys
-    
-    app = QApplication(sys.argv)
-    if not openDatabase() :
-        sys.exit(1)
 
-    query = QSqlQuery()
-    query.exec_("create table employee(id int, name varchar(20), city int, country int)")
-    query.exec_("insert into employee values(1, 'Espen', 5000, 47)")
+def initializeHistoryModel(model):
+    model.setQuery('select * from bill')
+    model.setHeaderData(0, Qt.Horizontal, "ID")
+    model.setHeaderData(2, Qt.Horizontal, "Namn")
+    model.setHeaderData(3, Qt.Horizontal, "Fakturan skapad")
+    model.setHeaderData(4, Qt.Horizontal, "Betald")
 
-    sys.exit(app.exec_())
+
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-
+from PyQt5.QtSql import QSqlQueryModel
 from ui_faktureramera import Ui_MainWindow
+
+import lib.db as db
 
 
 class FaktureraMeraWindow(QMainWindow):
 
-    def __init__(self, parent=None):
+   def __init__(self,  parent=None):
         """"""
         super(FaktureraMeraWindow, self).__init__(parent)
 
@@ -18,6 +20,15 @@ class FaktureraMeraWindow(QMainWindow):
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
-    faktureramera = FaktureraMeraWindow()
+
+    historyModel = QSqlQueryModel()
+    if not db.openDatabase():  
+        sys.exit(1)
+
+
+    db.initializeHistoryModel(historyModel)
+    faktureramera = FaktureraMeraWindow() 
+    faktureramera.ui.tableView.setModel(historyModel)
     faktureramera.show()    
+
     sys.exit(app.exec_())
